2eba440d23bd052e12e27080831725139e3d8887
Jim Shi <shiyilin2008@gmail.com>
2020-10-27 22:07:03 -0700

    Pillow tests: Adds out-of-boundary cropping tests.

--- PIL tests/2_crop.py
+++ PIL tests/2_crop.py
@@ -5,6 +5,7 @@ import os
 import PIL.Image as Image
 
 
+# MARK: - Normal cropping
 def test_crop_1():
     """
     Crop from center and top left.
@@ -45,8 +46,58 @@ def test_crop_2():
     pixel_cropped_image_3.save("out/abc_cropped_pixel_3.png")
 
 
+# MARK: - Out of boundary cropping
+def test_out_of_boundary_crop_1():
+    """
+    Crop an RGBA image using out-of-boundary coordinates.
+
+    Out of boundary pixels are transparent.
+    """
+    image: Image.Image = Image.open("../abc.png")
+    print(f"Original image mode: {image.mode}")    # RGBA
+    print(f"Original image size: {image.size}")    # (316, 316)
+    width, height = image.size
+
+    top_left_cropped_image_1: Image.Image = image.crop((-width / 4, -height / 4, width / 2, height / 2))
+    print(f"Top left cropped image 1 mode: {top_left_cropped_image_1.mode}")    # RGBA
+    print(f"Top left cropped image 1 size: {top_left_cropped_image_1.size}")    # (237, 237), 237 = 316 * 3 / 4
+    top_left_cropped_image_1.save("out/top_left_1.png")    # Works. The top left part of this image is 
+
+    top_left_cropped_image_2: Image.Image = image.crop((-width / 4, -height / 4, width * 3 / 4, height * 3 / 4))
+    print(f"Top left cropped image 2 size: {top_left_cropped_image_2.size}")    # (316, 316)
+    top_left_cropped_image_2.save("out/top_left_2.png")
+
+    bottom_right_cropped_image_1: Image.Image = image.crop((width / 2, height / 2, width * 5 / 4, height * 5 / 4))
+    print(f"Bottom right cropped image 1 size: {bottom_right_cropped_image_1.size}")    # (237, 237)
+    bottom_right_cropped_image_1.save("out/bottom_right_1.png")
+
+    bottom_right_cropped_image_2: Image.Image = image.crop((width * 3 / 4, height * 3 / 4, width * 7 / 4, height * 7 / 4))
+    print(f"Bottom right cropped image 2 size: {bottom_right_cropped_image_2.size}")    # (316, 316)
+    bottom_right_cropped_image_2.save("out/bottom_right_2.png")
+
+
+def test_out_of_boundary_crop_2():
+    """
+    Crop an RGB image using out-of-boundary coordinates.
+
+    Out of boundary pixels are black.
+    """
+    image: Image.Image = Image.open("../sfu1.png").convert("RGB")    # Although this image does not have transparent parts, it has a transparency channel.
+    print(f"Original image mode: {image.mode}")
+    print(f"Original image size: {image.size}")    # (1200, 675)
+
+    top_cropped_image_1: Image.Image = image.crop((0, -image.height / 2, image.width, image.height / 2))
+    print(f"Top cropped image 1 mode: {top_cropped_image_1.mode}")    # RGB
+    print(f"Top cropped image 1 size: {top_cropped_image_1.size}")    # (1200, 676) The old problem of even and old numbers.
+    top_cropped_image_1.save("out/top_1.png")
+
+
 if (__name__ == "__main__"):
     os.chdir(os.path.dirname(__file__))
     # print(f"Working directory: {os.getcwd()}")
+
     # test_crop_1()
-    test_crop_2()
+    # test_crop_2()
+
+    # test_out_of_boundary_crop_1()
+    test_out_of_boundary_crop_2()
