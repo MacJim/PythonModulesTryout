cda603b69ffc58c82b19daf884cbae2799d45222
Jim Shi <shiyilin2008@gmail.com>
2021-03-06 00:12:15 -0800

    Matplotlib: Adds log-scale histogram tests.

--- matplotlib_tests/histogram.py
+++ matplotlib_tests/histogram.py
@@ -30,9 +30,25 @@ def test_histogram():
     plt.clf()
 
 
+def _plot_log_histogram(x: np.ndarray, bins_count: int, plot_save_filename: str):
+    """
+    Source: https://stackoverflow.com/questions/47850202/plotting-a-histogram-on-a-log-scale-with-matplotlib
+    """
+    _, bins, _ = plt.hist(x, bins_count)
+    log_bins = np.logspace(np.log10(bins[0]), np.log10(bins[-1]), len(bins))
+    plt.clf()
+    plt.hist(x, bins=log_bins)
+    plt.xscale("log")
+    plt.savefig(plot_save_filename)
+
+
 def test_log_scale_histogram():
-    # TODO:
-    pass
+    uniform_distribution = np.random.uniform(1., 100., (210000,))
+    log_uniform_distribution = np.log10(uniform_distribution)
+
+    bins_count = 100
+
+    _plot_log_histogram(log_uniform_distribution, bins_count, "out/hist_log_uniform.png")
 
 
 # MARK: - Main
@@ -41,4 +57,5 @@ if (__name__ == "__main__"):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     print(f"Working directory: {os.getcwd()}")
 
-    test_histogram()
+    # test_histogram()
+    test_log_scale_histogram()
